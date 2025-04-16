fbdf76a24cebd0aa3e3e875c1c3f9cd8306d23c9
Anna Trybel <annakalinovich717@gmail.com>
2025-04-16 18:42:53 +0200

    Add view to capsule creation step 5

--- TimeCapsule/TimeCapsule/Controllers/CapsuleController.cs
+++ TimeCapsule/TimeCapsule/Controllers/CapsuleController.cs
@@ -1,14 +1,47 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TimeCapsule.Controllers
 {
     [Authorize]
+    [Route("TimeCapsule")]
     public class CapsuleController : TimeCapsuleBaseController
     {
+        private readonly IEmailSender _emailSender;
+        public CapsuleController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        [Route("Step5")]
+        public IActionResult Step5()
+        {
+            return View("~/Views/Capsule/CreateStep5.cshtml");
+        }
+
+        [Route("Step6")]
+        public IActionResult Step6()
+        {
+            return View("~/Views/Capsule/CreateStep6.cshtml");
+        }
+
+        [Route("SendEmail")]
+        public async Task<IActionResult> SendEmailAsync()
+        {
+            var receiver = "trybel.anna1@gmail.com";
+            var subject = "Test";
+            var message = "Test";
+
+            await _emailSender.SendEmailAsync(receiver, subject, message);
+
+            return View("~/Views/Capsule/CreateStep5.cshtml");
+        }
     }
 }
+
+
---
+++ TimeCapsule/TimeCapsule/Models/Dto/CapsuleDto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TimeCapsule.Models.DatabaseModels;
+
+namespace TimeCapsule.Models.Dto
+{
+    public class CapsuleDto
+    {
+        public int Id { get; set; }
+        public CapsuleType Type { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string Color { get; set; }
+        public string Introduction { get; set; }
+        public CapsuleQuestion CapsuleQuestions { get; set; }
+        public CapsuleAnswer CapsuleAnswers { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public IdentityUser CreatedByUser { get; set; }
+        public Status Status { get; set; }
+    }
+}
+
+
