85b3f1fca33d1e0dca5a10f5a50c2b95c24132da
Anna Trybel <annakalinovich717@gmail.com>
2025-04-21 13:17:06 +0200

    Merge CapsuleQuestionDto and CreateQuestionDto classes &&  CapsuleSectionDto and CreateSectionDto

--- TimeCapsule/TimeCapsule/Models/Dto/CapsuleQuestionDto.cs
+++ TimeCapsule/TimeCapsule/Models/Dto/CapsuleQuestionDto.cs
@@ -1,9 +1,23 @@
-﻿namespace TimeCapsule.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeCapsule.Models.Dto
 {
     public class CapsuleQuestionDto
     {
         public int Id { get; set; }
         public string QuestionText { get; set; }
+        public int SectionId { get; set; }
+        public string SectionName { get; set; }
         public int DisplayOrder { get; set; }
+    }
+
+    public class CreateQuestionDto
+    {
+        [Required(ErrorMessage = "Sekcja jest wymagana")]
+        public int SectionId { get; set; }
+
+        [Required(ErrorMessage = "Treść pytania jest wymagana")]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "Treść pytania musi mieć od 3 do 500 znaków")]
+        public string QuestionText { get; set; } = string.Empty;
     }
 }
--- TimeCapsule/TimeCapsule/Models/Dto/CapsuleSectionDto.cs
+++ TimeCapsule/TimeCapsule/Models/Dto/CapsuleSectionDto.cs
@@ -1,4 +1,5 @@
-﻿using TimeCapsule.Models.DatabaseModels;
+﻿using System.ComponentModel.DataAnnotations;
+using TimeCapsule.Models.DatabaseModels;
 
 namespace TimeCapsule.Models.Dto
 {
@@ -9,5 +10,15 @@ namespace TimeCapsule.Models.Dto
         public int DisplayOrder { get; set; }
         public CapsuleType CapsuleType { get; set; }
         public List<CapsuleQuestionDto> Questions { get; set; } = new List<CapsuleQuestionDto>();
+    }
+
+    public class CreateSectionDto
+    {
+        [Required(ErrorMessage = "Nazwa sekcji jest wymagana")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa sekcji musi mieć od 3 do 100 znaków")]
+        public string SectionName { get; set; }
+
+        [Required(ErrorMessage = "Typ kapsuły jest wymagany")]
+        public CapsuleType CapsuleType { get; set; }
     }
 }
