446edf66400ca0983512bb10aca995db66b6d8d4
Anna Trybel <annakalinovich717@gmail.com>
2025-04-25 20:48:21 +0200

    Prevent null reference exception in login process

--- TimeCapsule/TimeCapsule/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TimeCapsule/TimeCapsule/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,11 +21,13 @@ namespace TimeCapsule.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger, UserManager<IdentityUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         /// <summary>
@@ -84,9 +86,14 @@ namespace TimeCapsule.Areas.Identity.Pages.Account
             public bool RememberMe { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task OnGetAsync(string returnUrl = null, bool lockedOut = false)
         {
-            if (!string.IsNullOrEmpty(ErrorMessage))
+            if (lockedOut)
+            {
+                ModelState.AddModelError(string.Empty, "This account has been locked out. Please contact your administrator.");
+                _logger.LogWarning("Blocked user redirected to login page");
+            }
+            else if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
@@ -103,15 +110,23 @@ namespace TimeCapsule.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Home");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             if (ModelState.IsValid)
             {
                 // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true           	 
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -124,7 +139,8 @@ namespace TimeCapsule.Areas.Identity.Pages.Account
                 if (result.IsLockedOut)
                 {
                     _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
+                    ModelState.AddModelError(string.Empty, "User account locked out.");
+                    return Page();
                 }
                 else
                 {
@@ -138,3 +154,5 @@ namespace TimeCapsule.Areas.Identity.Pages.Account
         }
     }
 }
+
+
