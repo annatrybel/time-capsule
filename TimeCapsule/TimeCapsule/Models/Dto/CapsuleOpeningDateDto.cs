61d21df76ecd6d3072c123d9990fb1738c892bd0
Anna Trybel <annakalinovich717@gmail.com>
2025-05-27 17:02:44 +0200

    Add DTOs for capsule opening date management

---
+++ TimeCapsule/TimeCapsule/Models/Dto/CapsuleOpeningDateDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeCapsule.Models.Dto
+{
+    public class CapsuleOpeningDateDto
+    {
+        public int CapsuleId { get; set; }
+        public string Title { get; set; }
+        public DateTime CurrentOpeningDate { get; set; }
+    }
+
+    public class UpdateCapsuleOpeningDateDto
+    {
+        [Required(ErrorMessage = "Identyfikator kapsuły jest wymagany")]
+        public int CapsuleId { get; set; }
+
+        [Required(ErrorMessage = "Nowa data otwarcia jest wymagana")]
+        public DateTime NewOpeningDate { get; set; }
+    }
+}
+
+
